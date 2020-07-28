8362eb547b95572c4205bc9825e78aa3a71b9ff2
Diminich <hodarenokd@gmail.ru>
2020-07-28 10:10:11 +0300

    'v6'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import People from "./components/people/People";
-import MainWindow from "./components/mainWindow/MainWindow";
+import MainMenu from "./components/mainWindow/MainWindow";
 import {Route, Switch} from "react-router-dom";
 import Films from "./components/films/Films";
 import Starships from "./components/starships/Starships";
@@ -9,20 +9,19 @@ import Species from "./components/species/Species";
 import Planets from "./components/planets/Planets";
 import styles from './App.module.scss'
 
-
-
 function App() {
     return (
         <div className={styles.wrapper}>
+            <MainMenu/>
             <Switch>
-                <Route exact path = '/' render={() => <MainWindow/>}/>
+                <Route exact path = '/' render={() => <div/>}/>
                 <Route exact path = '/People' render={ () => <People/>}/>
                 <Route exact path = '/Films' render={() => <Films/>}/>
                 <Route exact path = '/Starships' render={() => <Starships/>}/>
                 <Route exact path = '/Vehicles' render={() => <Vehicles/>}/>
                 <Route exact path = '/Species' render={() => <Species/>}/>
                 <Route exact path = '/Planets' render={() => <Planets/>}/>
-                <Route exact path='*' render={() => <div>404 NOT FOUND</div>}/>
+                <Route exact path = '*' render={() => <div className={styles.notFound}>404 NOT FOUND</div>}/>
             </Switch>
         </div>
     );
--- src/components/mainWindow/MainWindow.js
+++ src/components/mainWindow/MainWindow.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import cn from 'classnames';
 import styles from './MainWindow.module.scss'
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 
-function MainWindow() {
+function MainMenu() {
+    const {pathname} = useLocation();
     return (
-        <nav className={styles.wrapper}>
-            <div className={styles.peopleFilmsStarships}>
+        <nav className={cn(pathname === '/' ? styles.wrapper : styles.smallWrapper)}>
+            <div className={cn(pathname === '/' ? styles.peopleFilmsStarships : styles.smallPeopleFilmsStarships)}>
             <NavLink className={styles.lineHighlight} to = '/People'>People</NavLink>
             <NavLink className={styles.lineHighlight} to = '/Films'>Films</NavLink>
             <NavLink className={styles.lineHighlight} to = '/Starships'>Starships</NavLink>
             </div>
-            <div className={styles.vehiclesSpeciesPlanets}>
+            <div className={cn(pathname === '/' ? styles.vehiclesSpeciesPlanets : styles.smallVehiclesSpeciesPlanets)}>
             <NavLink className={styles.lineHighlight} to = '/Vehicles'>Vehicles</NavLink>
             <NavLink className={styles.lineHighlight} to = '/Species'>Species</NavLink>
             <NavLink className={styles.lineHighlight} to = '/Planets'>Planets</NavLink>
@@ -19,4 +21,4 @@ function MainWindow() {
     );
 }
 
-export default MainWindow;
+export default MainMenu;
