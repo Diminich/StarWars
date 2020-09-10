51bd3d81c3a11cdb7e6d36db00c7228654021fd5
Diminich <hodarenokd@gmail.com>
2020-09-10 17:12:06 +0300

    Updates

---
+++ precache-manifest.44c420ef3280c7b496be6fb9143f4db0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b8bde27ffde6083402f0d255a29876",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "6a3f1485ecd7e194d020",
+    "url": "/StarWars/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "558f548c5583d5d45553",
+    "url": "/StarWars/static/css/main.01b01f61.chunk.css"
+  },
+  {
+    "revision": "6a3f1485ecd7e194d020",
+    "url": "/StarWars/static/js/2.0c8b1197.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/StarWars/static/js/2.0c8b1197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558f548c5583d5d45553",
+    "url": "/StarWars/static/js/main.3ae3f709.chunk.js"
+  },
+  {
+    "revision": "d2f2f7b328f7a0187ad3",
+    "url": "/StarWars/static/js/runtime-main.4b33b6cf.js"
+  },
+  {
+    "revision": "89c1716c6f48177e1de1fa8ece1ca552",
+    "url": "/StarWars/static/media/FL.89c1716c.jpg"
+  },
+  {
+    "revision": "b6ecefae17ad31e57303183d9a20a76f",
+    "url": "/StarWars/static/media/MW.b6ecefae.jpg"
+  },
+  {
+    "revision": "ef00dfcc59b7c30f903334e7c437cf96",
+    "url": "/StarWars/static/media/PE.ef00dfcc.jpg"
+  },
+  {
+    "revision": "af3a710defc40d48826bbb5ca64bddc8",
+    "url": "/StarWars/static/media/PL.af3a710d.jpg"
+  },
+  {
+    "revision": "aa65a53a8f5449705b95cb9f945e1bdb",
+    "url": "/StarWars/static/media/SD.aa65a53a.jpg"
+  },
+  {
+    "revision": "c167f226e5a4447278e45d3d6941c52b",
+    "url": "/StarWars/static/media/SP.c167f226.jpg"
+  },
+  {
+    "revision": "563d3adb3fdc368d87def3c10b1209d5",
+    "url": "/StarWars/static/media/VE.563d3adb.jpg"
+  }
+]);
