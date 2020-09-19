bd8890bfce902c597d08ece0da784b97326260a2
Diminich <hodarenokd@gmail.com>
2020-09-19 20:34:45 +0300

    TS 'v1'

---
+++ src/Types/TypeFilms.tsx
@@ -0,0 +1,31 @@
+export type FilmsSearchType = {
+    characters: string
+    created: string
+    director: string
+    edited: string
+    episode_id: string
+    planets: string
+    producer: string
+    release_date: string
+    species: string
+    starships: string
+    title: string
+    url: string
+    vehicles: string
+}
+
+export type FilmsSpecificType = {
+    characters: string
+    created: string
+    director: string
+    edited: string
+    episode_id: string
+    planets: string
+    producer: string
+    release_date: string
+    species: string
+    starships: string
+    title: string
+    url: string
+    vehicles: string
+}
---
+++ src/Types/TypeVechiles.tsx
@@ -0,0 +1,37 @@
+export type VechilesSearchType = {
+    cargo_capacity: string
+    consumables: string
+    cost_in_credits: string
+    created: string
+    crew: string
+    edited: string
+    length: string
+    manufacturer: string
+    max_atmosphering_speed: string
+    model: string
+    name: string
+    passengers: string
+    pilots: string
+    films: string
+    url: string
+    vehicle_class: string
+}
+
+export type VechilesSpecificType = {
+    cargo_capacity: string
+    consumables: string
+    cost_in_credits: string
+    created: string
+    crew: string
+    edited: string
+    length: string
+    manufacturer: string
+    max_atmosphering_speed: string
+    model: string
+    name: string
+    passengers: string
+    pilots: string
+    films: string
+    url: string
+    vehicle_class: string
+}
